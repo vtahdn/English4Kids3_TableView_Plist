80f73bc8e182f8f8b2091a81fc389bc7c57fac3d
Đoàn Việt <vtahdn@gmail.com>
2018-09-26 14:53:12 +0700

    Initial Commit

---
+++ English4KidsTests/English4KidsTests.swift
@@ -0,0 +1,36 @@
+//
+//  English4KidsTests.swift
+//  English4KidsTests
+//
+//  Created by macbook on 9/26/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import XCTest
+@testable import English4Kids
+
+class English4KidsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
